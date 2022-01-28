91c4b7567e4d021f1d208971954c2a314bab73de
jay <jieyuan1@ualberta.ca>
2022-01-27 20:47:39 -0800

    passed all tests

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 #  coding: utf-8 
 import socketserver
+import re
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -35,15 +37,32 @@ class MyWebServer(socketserver.BaseRequestHandler):
         #TODO
         message = "HTTP/1.1 "
         path = self.get_path(self.data.decode("utf-8"))
-        body = self.get_body(path)
-        if self.check_get_method(self.data.decode("utf-8")):
-            #Handle the GET request
-            message += "200 OK\n"
-        else:
-            #Other requests -- 405 Method Not Allowed
+        print("path is", path)
+        body = ""
+        content_type = "text/plain"
+        #Other requests -- 405 Method Not Allowed
+        if not self.check_get_method(self.data.decode("utf-8")):
             message += "405 Method Not Allowed\n"
+        else:
+            if path:
+                # validate the path
+                status_code = self.check_path(path)
+                print("status code is: ",status_code)
+                if status_code == 200:
+                    body = self.get_body(path)
+                    content_type = self.check_file(path)
+                    message += "200 OK\n"
+                elif status_code == 301:
+                    message = message + "301 Moved Permanently\nLocation: http://127.0.0.1:8080" + path +"/\n"
+                elif status_code == 404:
+                    message += "404 Not Found\n"
 
-        message = message + 'Content-Length: ' + str(len(body))
+            else:
+                # no path exist
+                message += "404 Not Found\n"
+        
+
+        message = message + 'Content-Type: ' + content_type + '\nContent-Length: ' + str(len(body)+1)
         #Send back response of whatever
         self.request.sendall(bytearray(message+"\r\n\r\n"+body+'\n','utf-8'))
 
@@ -56,19 +75,65 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
     #Get the path of the request
     def get_path(self, request):
-        return "/"
-        #TODO handle 301 error
-        #TODO handle 404 error
+        path = re.search('/.*? HTTP', request)
+        if path is not None:
+            path = path.group(0).replace(" HTTP","")
+            # print('path\n',path)
+            return path
+        else:
+            return None
+
+    #Check if the path is valid
+    def check_path(self, path):
+        path = "www"+path
+        #for security issue
+        current_path = os.getcwd()
+        abs_path = os.path.abspath(path)
+        if os.path.commonpath((current_path, abs_path)) != current_path:
+            return 404
+        # print("current_path and abs_path:",current_path, abs_path)
+
+        if os.path.exists(path):
+            if os.path.isfile(path):
+                return 200
+        #301 error
+        if path[-1] != '/':
+            path += '/'
+            if os.path.exists(path):
+                return 301
+            else:
+                print("path not exist after adding /: ",path)
+                return 404
+        else:
+            #404 error
+            if not os.path.exists(path):
+                print("path not exist: ",path)
+                return 404
+        return 200
+
+    #Check file mimetype
+    def check_file(self, path):
+        the_path = path.split('.')
+        if the_path[-1] == "css":
+            return "text/css"
+        else:
+            return "text/html"
+
 
     #Get the body of the response
     def get_body(self, path):
         try:
-            with open('www/index.html','r') as file:
+            with open('www'+path,'r') as file:
                 text = ''.join(file.readlines())
-                print('text\n',text)
+                # print('text\n',text)
+                return text
+        except IsADirectoryError:
+            with open('www'+path+'index.html','r') as file:
+                text = ''.join(file.readlines())
+                # print('text\n',text)
                 return text
         except Exception as e:
-            print('error\n',e)
+            # print('error\n',e)
             return e
 
 if __name__ == "__main__":
